daaa7d92b1ff3b7ceba19ece3994e26ff8ea2e68
areg98 <areg98>
2022-11-10 20:35:01 +0400

    refactor

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,24 @@
-import MyList.MyList;
 import MyList.MyArrayList;
+import MyList.MyLinkedList;
 import MyList.MyVector;
 import MyList.MyStack;
 import MyString.MyString;
+import java.util.LinkedList;
 
 
 public class Main {
     public static void main(String[] args) {
-        MyList<Integer> list = new MyVector<>();
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        list.add(1,5);
+        MyLinkedList<Integer> list = new MyLinkedList();
+        list.add(2);
+        list.add(5);
+        list.add(3);
+        list.add(5);
+        list.add(4);
+        list.add(5);
+        list.add(5);
         System.out.println(list);
-        list.removeRange(0,2);
-        System.out.println(list);
+        System.out.println(list.size());
         System.out.println(list.get(0));
-        System.out.println(list.contains(1));
         System.out.println(list.isEmpty());
-        System.out.println(list.indexOf(1));
-
-        list.add(1,2);
-        System.out.println(list);
-        list.set(1,5);
-        System.out.println(list);
-        System.out.println(list.get(0));
-        System.out.println(list.lastIndexOf(1));
-
-        MyList list1;
-        list1 = list.subList(0,2);
-        System.out.println(list1);
-        MyList clone;
-        clone = list1.clone();
-        System.out.println(clone);
-        clone.sort();
-        System.out.println(clone);
-
     }
 }
